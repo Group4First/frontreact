3678938e9bd193be3fb38d5fcacd9ca238da0f0e
fic <Group4First@gmail.com>
2024-02-26 14:48:30 -0500

    updatre registroemmpresas

--- src/pages/RegistroEmpresa.jsx
+++ src/pages/RegistroEmpresa.jsx
@@ -10,15 +10,25 @@ export function RegistroEmpresa() {
     const handleDocumentClick = (documentType) => {
         setSelectedDocumentType(documentType);
     };
-    
+
     const [Razonsocial, setRazonsocial] = useState('')
     const [NumIdentificacion, setNumIdentificacion] = useState('')
+    const [Tipodocumento, setTipodocumento] = useState('')
     const [DV, setDV] = useState('')
     const [CIIU, setCIIU] = useState('')
+    const [Actividadeconomica, setActividadeconomica] = useState('')
     const [Direccion, setDireccion] = useState('')
     const [Municipio, setMunicipio] = useState('')
     const [Telefono, setTelefono] = useState('')
     const [Fax, setFax] = useState('')
+    const [Correo, setCorreo] = useState('')
+    const [Representantelegal, setRepresentantelegal] = useState('')
+    const [CCrepresentantelegal, setCCrepresentantelegal] = useState('')
+    const [Cajadecompensacion, setCajadecompensacion] = useState('')
+    const [Escrituraconstitucion, setEscrituraconstitucion] = useState('')
+    const [Notaria, setNotaria] = useState('')
+    const [Ciudad, setCiudad] = useState('')
+    const [Fecha, setFecha] = useState('')
 
 
 
@@ -55,13 +65,13 @@ export function RegistroEmpresa() {
                 <section className="mt-5 ml-5 overflow-y-auto " style={{ maxHeight: "calc(100vh - 100px)" }}>
 
 
-                    <div className=" flex flex-wrap centered">
+                    <div className=" flex flex-wrap centered mt-5">
                         <div className="bg-white h-12 w-[284px] rounded-xl border-2 border-vgray flex items-center text-vgray2 px-3 mr-4 mt-4">
-                            <input type="text" required="true" placeholder="Razon social" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
+                            <input onChange={(event) => { setRazonsocial(event.target.value); }} type="text" required="true" placeholder="Razon social" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
                         </div>
                         <div className="flex centered flex-wrap mr-4 mt-4">
                             <div className={`bg-white h-12 w-[284px] border-2 border-vgray flex items-center font-semibold text-vgray2 px-3 ${window.innerWidth <= 425 ? 'rounded-t-xl border-t-2 border-l-2 border-r-2 border-b-0' : 'rounded-l-xl border-2'}`}>
-                                <input type="text" placeholder="Numero de documento" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-full md:w-[284px] text-center " />
+                                <input onChange={(event) => { setNumIdentificacion(event.target.value); }} type="text" placeholder="Numero de documento" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-full md:w-[284px] text-center " />
                             </div>
                             <div className={`bg-white h-12 border-vgray flex items-center text-vgray2 px-3 border-l-0 border-r-0 ${window.innerWidth <= 425 ? 'w-[142px] block md:flex border-l-2 rounded-bl-xl border-2' : 'block md:flex rounded-1-xl border-t-2 border-b-2'}`} style={{ backgroundColor: selectedDocumentType === 'NIT' ? '#BAEDBD' : 'white' }} onClick={() => handleDocumentClick('NIT')}>
                                 <button className="font-semibold text-black p-2">NIT</button>
@@ -71,63 +81,66 @@ export function RegistroEmpresa() {
                             </div>
                         </div>
                         <div className="bg-white h-12 w-26 rounded-xl border-2 border-vgray flex items-center text-vgray2 px-3 mr-4 mt-4">
-                            <input type="text" placeholder="DV" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-8" />
+                            <input onChange={(event) => { setDV(event.target.value); }} type="text" placeholder="DV" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-8" />
                         </div>
-                        <div className="bg-white h-12  w-32 rounded-xl border-2 border-vgray flex items-center text-vgray2 px-3 mt-4">
-                            <input type="text" placeholder="CIIU" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-20 text-center" />
+
+                    </div>
+
+                    <div className=" flex flex-wrap mt-4 centered">
+                        <div className="bg-white h-12  w-32 rounded-xl border-2 border-vgray flex items-center text-vgray2 px-3 mr-4 mt-4">
+                            <input onChange={(event) => { setCIIU(event.target.value); }} type="text" placeholder="CIIU" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-20 text-center" />
+                        </div>
+                        <div className="bg-white h-12 w-[284px] rounded-xl border-2 border-vgray flex items-center text-vgray2 px-3 mr-4 mt-4">
+                            <input onChange={(event) => { setActividadeconomica(event.target.value); }} type="text" placeholder="Actividad economica" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
                         </div>
                     </div>
 
                     <div className=" flex flex-wrap mt-4 centered">
                         <div className="bg-white h-12 w-[284px] rounded-xl border-2 border-vgray flex items-center text-vgray2 px-3 mr-4 mt-4">
-                            <input type="text" placeholder="Direccion" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
+                            <input onChange={(event) => { setDireccion(event.target.value); }} type="text" placeholder="Direccion" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
                         </div>
                         <div className="bg-white h-12 w-[284px] rounded-xl border-2 border-vgray flex items-center text-vgray2 px-3 mr-4 mt-4">
-                            <input type="text" placeholder="Municipio" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
+                            <input onChange={(event) => { setMunicipio(event.target.value); }} type="text" placeholder="Municipio" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
                         </div>
                     </div>
 
                     <div className=" flex flex-wrap mt-4 centered">
                         <div className="bg-white h-12 w-[284px] rounded-xl border-2 border-vgray flex items-center text-vgray2 px-3 mr-4 mt-4">
-                            <input type="text" placeholder="Telefono" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
+                            <input onChange={(event) => { setTelefono(event.target.value); }} type="text" placeholder="Telefono" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
                         </div>
                         <div className="bg-white h-12 w-[284px] rounded-xl border-2 border-vgray flex items-center text-vgray2 px-3 mr-4 mt-4">
-                            <input type="text" placeholder="Fax" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
+                            <input onChange={(event) => { setFax(event.target.value); }} type="text" placeholder="Fax" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
                         </div>
                         <div className="bg-white h-12 w-[284px] rounded-xl border-2 border-vgray flex items-center text-vgray2 px-3 mt-4">
-                            <input type="text" placeholder="Correo electronico" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
+                            <input onChange={(event) => { setCorreo(event.target.value); }} type="text" placeholder="Correo electronico" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
                         </div>
                     </div>
 
                     <div className=" flex flex-wrap mt-4 centered">
                         <div className="bg-white h-12 w-[284px] rounded-xl border-2 border-vgray flex items-center text-vgray2 px-3 mr-4 mt-4">
-                            <input type="text" placeholder="Representante legal" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
+                            <input onChange={(event) => { setRepresentantelegal(event.target.value); }} type="text" placeholder="Representante legal" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
                         </div>
                         <div className="bg-white h-12 w-[284px] rounded-xl border-2 border-vgray flex items-center text-vgray2 px-3 mr-4 mt-4">
-                            <input type="text" placeholder="Cedula R.P" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
+                            <input onChange={(event) => { setCCrepresentantelegal(event.target.value); }} type="text" placeholder="Cedula R.P" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
                         </div>
-
-                    </div>
-                    <div className=" flex flex-wrap mt-4 centered">
                         <div className="bg-white h-12 w-[284px] rounded-xl border-2 border-vgray flex  text-vgray2 px-3 mr-4 mt-4">
-                            <input type="text" placeholder="Caja de compensacion" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
+                            <input onChange={(event) => { setCajadecompensacion(event.target.value); }}type="text" placeholder="Caja de compensacion" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
                         </div>
-
                     </div>
 
                     <div className=" flex flex-wrap mt-4 centered">
                         <div className="bg-white h-12 w-[284px] rounded-xl border-2 border-vgray flex  text-vgray2 px-3 mr-4 mt-4 ">
-                            <input type="text" placeholder="Escritura constitucion" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
+                            <input onChange={(event) => { setEscrituraconstitucion(event.target.value); }} type="text" placeholder="Escritura constitucion" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
                         </div>
                         <div className="bg-white h-12 w-[284px] rounded-xl border-2 border-vgray flex  text-vgray2 px-3 mr-4 mt-4 ">
-                            <input type="text" placeholder="Notaria" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
+                            <input onChange={(event) => { setNotaria(event.target.value); }} type="text" placeholder="Notaria" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
                         </div>
                         <div className="bg-white h-12 w-[284px] rounded-xl border-2 border-vgray flex  text-vgray2 px-3 mr-4 mt-4 ">
-                            <input type="text" placeholder="Ciudad" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
+                            <input onChange={(event) => { setCiudad(event.target.value); }} type="text" placeholder="Ciudad" className="placeholder:font-semibold placeholder:text-vgray2 outline-none text-black font-semibold ml-3 w-[284px] text-center" />
                         </div>
                         <div className="bg-white h-12 w-[284px] rounded-xl border-2 border-vgray flex text-vgray2 px-3 mr-4 mt-4 mb-20">
                             <label htmlFor="fecha" className="text-vgray2 font-semibold flex-grow mt-[10px] ml-4">Fecha</label>
-                            <input type="date" id="fecha" placeholder="Fecha" className="outline-none text-black font-semibold w-[150px] " />
+                            <input onChange={(event) => { setFecha(event.target.value); }}type="date" id="fecha" placeholder="Fecha" className="outline-none text-black font-semibold w-[150px] " />
                         </div>
 
                     </div>
---
+++ src/requests/postempresas.js
@@ -0,0 +1,32 @@
+import { newRequest } from "./newRequest"
+
+export async function postbussiness(numidentificacion,razonsocial,tipodocumento,dv,direccion,municipio,telefono,fax ,actividadeconomica,ciiu,correo,representantelegal,
+                                    ccrepresentante,cajadecompensacion,numescrituraconstitucion,numnotaria,ciudad,fecha){
+                       
+  const body ={
+    numidentificacion: numidentificacion,
+    razonsocial: razonsocial,
+    tipodocumento: tipodocumento,
+    dv: dv,
+    direccion: direccion,
+    municipio: municipio,
+    telefono: telefono,
+    fax: fax,
+    actividadeconomica: actividadeconomica,
+    ciiu: ciiu,
+    correo: correo,
+    representantelegal: representantelegal,
+    ccrepresentante: ccrepresentante,
+    cajadecompensacion: cajadecompensacion,
+    numescrituraconstitucion: numescrituraconstitucion,
+    numnotaria: numnotaria,
+    ciudad: ciudad,
+    fecha: fecha
+  }
+  
+
+  const url = new URL('http://localhost:8080/api/user/create');
+
+  return await newRequest({url, body, method: 'POST'})
+}
+
