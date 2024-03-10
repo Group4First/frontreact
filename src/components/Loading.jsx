fc6db757ad0b04c3948ad0a63de701aeac8a2d28
Group4First <Group4First@gmail.com>
2024-03-10 18:43:22 -0500

    update loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,9 @@
 import { Loader2 } from "lucide-react";
 
-export function Loading ({size = 24}) {
+export function Loading ({size = 24 ,color = '#39A900'}) {
   return (
     <div className="animate-spin">
-      <Loader2 color="#38A900" size={size} strokeWidth={2.8}/>
+      <Loader2 color={color} size={size} strokeWidth={2.8}/>
     </div> 
   )
 }
