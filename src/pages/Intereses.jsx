c44522052e9c39e480d65013add09a4ade25328f
Group4First <Group4First@gmail.com>
2024-03-11 17:55:44 -0500

    Update Intereses.jsx

--- src/pages/Intereses.jsx
+++ src/pages/Intereses.jsx
@@ -13,7 +13,7 @@ export function Intereses() {
     const [lintereses, setLintereses] = useState([]);
     const [searchTerm, setSearchTerm] = useState('')
     const [currentPage, setCurrentPage] = useState(0)
-    const {activeAlert} = useGlobalContext()
+    const { activeAlert } = useGlobalContext()
 
     useEffect(() => {
         async function fetchData() {
@@ -42,21 +42,28 @@ export function Intereses() {
 
     if (lintereses === null) {
         return <div className="w-full max-w-fu h-svh flex justify-center items-center">
-            <Loading size={50}/>
-        </div> 
+            <Loading size={50} />
+        </div>
     }
-    
+
     if (lintereses !== null) return (
         <div className="w-full max-w-fu h-svh overflow-y-auto">
             <h1 className="text-vgreen font-semibold px-16 mt-4 text-xl">Lista de intereses</h1>
 
             <section className=" flex justify-center">
-                <div className=" w-11/12 flex items-center justify-between mt-10 ">
-                    <div className="h-12 w-60 rounded-xl border-2 bg-[#E6E5E5] flex items-center text-vgray2 justify-center">
-                        <Search size={20} color="#7D7D7D" />
-                        <input onChange={(event) => { setSearchTerm(event.target.value); }} type="text" placeholder="Buscar" className=" bg-[#E6E5E5] placeholder:font-medium placeholder:text-[#7D7D7D] outline-none text-black font-semibold ml-3 w-44" />
+                <div className=" w-11/12  flex xl:items-end max-xl:items-center justify-between mt-10 max-xl:flex-col max-xl:gap-10 max-xl:justify-center">
+                    <div className="flex flex-wrap mt-4 max-xl:justify-center">
+                        <div className="bg-white h-12 w-[320px] rounded-xl border-2 border-vgray flex items-center text-vgray2 px-3 mr-4 mt-6 centered-full">
+                            <label htmlFor="Mes" className="text-vgray2 font-semibold flex-grow ml-4">Mes</label>
+                            <input onChange={(event) => { setSearchTerm(event.target.value); }} type="date" id="Mes" placeholder="Mes" className="outline-none text-black font-semibold w-[150px]" />
+                        </div>
+                        <div className="bg-white h-12 w-[320px] rounded-xl border-2 border-vgray flex items-center text-vgray2 px-3 mr-4 mt-6 centered-full">
+                            <label htmlFor="Año" className="text-vgray2 font-semibold flex-grow ml-4">Año</label>
+                            <input onChange={(event) => { setSearchTerm(event.target.value); }} type="date"    id="Año" placeholder="Año" className="outline-none text-black font-semibold w-[150px]" />
+                        </div>
                     </div>
-                    <button className="px-4 py-2 bg-vgreen text-white font-medium text-sm rounded-lg flex gap-2" onClick={() => {
+
+                    <button className="px-4 py-2 xl:mb-2 bg-vgreen text-white font-medium text-sm rounded-lg flex gap-2" onClick={() => {
                         navigate('/intereses/registrointereses')
                     }}>
                         <Plus size={20} color="#FFFFFF" />
@@ -67,7 +74,8 @@ export function Intereses() {
 
 
             <section className="flex flex-col items-center relative overflow-x-auto">
-                <div className="max-xl:scale-0 h-14 w-11/12 bg-white rounded-xl mb-3 grid items-center px-3 mt-6 grid-cols-6">
+                <div className="max-xl:scale-0 h-14 w-11/12 bg-white rounded-xl mb-3 grid items-center px-3 mt-6 grid-cols-7">
+                    <h1 className=" text-vgraylight font-medium">Año</h1>
                     <h1 className=" text-vgraylight font-medium">Mes</h1>
                     <h1 className=" text-vgraylight font-medium text-center">Desde</h1>
                     <h1 className=" text-vgraylight font-medium text-center" >Hasta</h1>
@@ -77,14 +85,15 @@ export function Intereses() {
                 </div>
                 <div className="w-full flex xl:items-center xl:flex-col max-xl:justify-center max-xl:flex-row max-xl:flex-wrap max-xl:-translate-y-16">
                     {intereses?.map((interes, index) => (
-                        <div key={index} className={`text-vgraydark font-semibold bg-white rounded-xl items-center px-3 mt-5 max-xl:max-w-[280px] max-xl:w-[280px] max-xl:p-5 max-xl:rounded-2xl max-xl:mx-4 xl:grid xl:text-center xl:grid-cols-6 xl:h-14 xl:w-11/12`}>
-                           
-                           <h1 className="text-left"> <span className="xl:text-[0px] xl:text-transparent xl:scale-0 text-black"> Mes: </span>{interes.mes}</h1>
-                           <h1 className=""> <span className="xl:text-[0px] xl:text-transparent xl:scale-0 text-black"> Desde: </span>{interes.fechainicio}</h1>
-                           <h1 className=""> <span className="xl:text-[0px] xl:text-transparent xl:scale-0 text-black"> Hasta: </span>{interes.fechafin}</h1>
-                           <h1 className=""> <span className="xl:text-[0px] xl:text-transparent xl:scale-0 text-black"> Tasa legal usura: </span>{interes.tasa}%</h1>
-                           <h1 className=""> <span className="xl:text-[0px] xl:text-transparent xl:scale-0 text-black"> Tasa mensual: </span>{interes.mensual}%</h1>
-                           <h1 className=""> <span className="xl:text-[0px] xl:text-transparent xl:scale-0 text-black"> Tasa diaria: </span>{interes.diario}%</h1>
+                        <div key={index} className={`text-vgraydark font-semibold bg-white rounded-xl items-center px-3 mt-5 max-xl:max-w-[280px] max-xl:w-[280px] max-xl:p-5 max-xl:rounded-2xl max-xl:mx-4 xl:grid xl:text-center xl:grid-cols-7 xl:h-14 xl:w-11/12`}>
+
+                            <h1 className="text-left"> <span className="xl:text-[0px] xl:text-transparent xl:scale-0 text-black"> Año: </span>{interes.año}</h1>
+                            <h1 className="text-left"> <span className="xl:text-[0px] xl:text-transparent xl:scale-0 text-black"> Mes: </span>{interes.mes}</h1>
+                            <h1 className=""> <span className="xl:text-[0px] xl:text-transparent xl:scale-0 text-black"> Desde: </span>{interes.fechainicio}</h1>
+                            <h1 className=""> <span className="xl:text-[0px] xl:text-transparent xl:scale-0 text-black"> Hasta: </span>{interes.fechafin}</h1>
+                            <h1 className=""> <span className="xl:text-[0px] xl:text-transparent xl:scale-0 text-black"> Tasa legal usura: </span>{interes.tasa}%</h1>
+                            <h1 className=""> <span className="xl:text-[0px] xl:text-transparent xl:scale-0 text-black"> Tasa mensual: </span>{interes.mensual}%</h1>
+                            <h1 className=""> <span className="xl:text-[0px] xl:text-transparent xl:scale-0 text-black"> Tasa diaria: </span>{interes.diario}%</h1>
                         </div>
                     ))}
 
